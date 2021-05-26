dff7767a2fd94f61c8a3cc33321cd6aa9923c6a0
mykyta <mykytazviahin@yahoo.com>
2021-05-26 21:17:42 +0300

    basic logic

--- matches/src/App.js
+++ matches/src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [matchesLeft, setMatchesLeft] = useState(25)
+  const [matchesLeftYours, setMatchesLeftYours] = useState(0)
+  const [matchesLeftAI, setMatchesLeftAI] = useState(0)
+
+  const takeMatches = (number) => {
+    setMatchesLeft(matchesLeft => matchesLeft - number)
+    setMatchesLeftYours(matchesLeftYours => matchesLeftYours + number)
+    takeMatchesAI(matchesLeft-number)
+  }
+
+  const takeMatchesAI = (number) => {
+    console.log((number - 3) % 4);
+    console.log((number - 1) % 4);
+    if ((((number - 3) % 4 === 0) || ((number - 3) % 4 === 1))) {
+      setMatchesLeft(matchesLeft => matchesLeft - 3)
+      setMatchesLeftAI(matchesLeftAI => matchesLeftAI + 3)
+    }
+    if ((((number - 1) % 4 === 0) || ((number - 1) % 4 === 1))) {
+      setMatchesLeft(matchesLeft => matchesLeft - 1)
+      setMatchesLeftAI(matchesLeftAI => matchesLeftAI + 1)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>GamePage</div>
+      <div>matches {matchesLeft}</div>
+      <button onClick={() => {takeMatches(3)}}>3 matches</button>
+      <button onClick={() => {takeMatches(2)}}>2 matches</button>
+      <button onClick={() => {takeMatches(1)}}>1 matches</button>
+      <div>matchesyours {matchesLeftYours}</div>
+      <div>matchesAI {matchesLeftAI}</div>
+    </>
   );
 }
 

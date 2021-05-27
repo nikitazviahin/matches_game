6d938e5b56be2bc1a295190aace20645195bb369
mykyta <mykytazviahin@yahoo.com>
2021-05-27 13:22:07 +0300

    finish basic logic and add styles

--- matches/src/App.js
+++ matches/src/App.js
@@ -1,20 +1,39 @@
 import React, { useState } from 'react';
+import './App.css';
 
 function App() {
 
   const [matchesLeft, setMatchesLeft] = useState(25)
   const [matchesLeftYours, setMatchesLeftYours] = useState(0)
   const [matchesLeftAI, setMatchesLeftAI] = useState(0)
+  
+  let isGameEnded = false;
+
+  const calculateWinner = (number, matchesPlayer) => {
+    if (number === 0) {
+      if (matchesPlayer % 2 === 0) {
+        return 'You are winner';
+      } else {
+        return 'AI is winner';
+      }
+    } else {
+      return 'Take matches';
+    }
+  }
+
+  const winner = calculateWinner(matchesLeft, matchesLeftYours)
+
+  if (winner === 'You are winner' || winner === 'AI is winner') {
+    isGameEnded = true
+  }
 
   const takeMatches = (number) => {
     setMatchesLeft(matchesLeft => matchesLeft - number)
     setMatchesLeftYours(matchesLeftYours => matchesLeftYours + number)
-    takeMatchesAI(matchesLeft-number)
+    takeMatchesAI(matchesLeft - number)
   }
 
   const takeMatchesAI = (number) => {
-    console.log((number - 3) % 4);
-    console.log((number - 1) % 4);
     if ((((number - 3) % 4 === 0) || ((number - 3) % 4 === 1))) {
       setMatchesLeft(matchesLeft => matchesLeft - 3)
       setMatchesLeftAI(matchesLeftAI => matchesLeftAI + 3)
@@ -26,15 +45,27 @@ function App() {
   }
 
   return (
-    <>
-      <div>GamePage</div>
-      <div>matches {matchesLeft}</div>
-      <button onClick={() => {takeMatches(3)}}>3 matches</button>
-      <button onClick={() => {takeMatches(2)}}>2 matches</button>
-      <button onClick={() => {takeMatches(1)}}>1 matches</button>
-      <div>matchesyours {matchesLeftYours}</div>
-      <div>matchesAI {matchesLeftAI}</div>
-    </>
+    <div className='App'>
+        <div className='Title'>
+          <p>Matches Game</p>
+        </div>
+        <div className='Subtitle'>
+          <p>I found no match emoji so used pencil instead</p>
+        </div>
+        <div className='Matches'>{matchesLeft} ✏️</div>
+        <div className='Winner'>{winner}</div>
+        {isGameEnded ? <div className='GameEnded'>Game Ended</div> :
+          <div className='ButtonsPanel'>
+            {(matchesLeft >= 3) ? <div className='ButtonPadding'><button className='ChooseButton' onClick={() => {takeMatches(3)}}>✏️ ✏️ ✏️</button></div> : 
+              <div className='ButtonPadding'><button className='ChooseButton' disabled onClick={() => {takeMatches(3)}}>Not enough matches</button></div>}
+            {(matchesLeft >= 2) ? <div className='ButtonPadding'><button className='ChooseButton' onClick={() => {takeMatches(2)}}>✏️ ✏️</button></div> : 
+              <div className='ButtonPadding'><button className='ChooseButton' disabled onClick={() => {takeMatches(2)}}>Not enough matches</button></div>}
+            {(matchesLeft >= 1) ? <div className='ButtonPadding'><button className='ChooseButton' onClick={() => {takeMatches(1)}}>✏️</button></div> : 
+              <div className='ButtonPadding'><button className='ChooseButton' disabled onClick={() => {takeMatches(1)}}>Not enough matches</button></div>}
+          </div>}
+        <div>Your matches {matchesLeftYours}</div>
+        <div>AI's matches {matchesLeftAI}</div>
+    </div>
   );
 }
 

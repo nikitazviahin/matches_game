6395f09ef3eae20d94538004d5d647da76f3281c
mykyta <mykytazviahin@yahoo.com>
2021-05-28 23:41:27 +0300

    add two regimes and change logic

--- matches/src/App.js
+++ matches/src/App.js
@@ -1,28 +1,28 @@
 import React, { useState } from 'react';
 import './App.css';
-import PlayerSecond from './pages/playerSecond';
 import PlayerFirst from './pages/playerFirst';
 
 function App() {
 
-  const [chosen, setChosen] = useState(false);
+  const [chosen, setChosen] = useState(true);
+  const [gameStarted, setGameStarted] = useState(false)
 
   return (
     <div className='ChooseRegime'>
-      <p>Choose who will go first</p>
-      <button className='ChooseButton' onClick={() => setChosen(true)}>Player first</button>
-      <button className='ChooseButton' onClick={() => setChosen(false)}>Player second</button>
-      {chosen ?
-              <> 
-                <p>Player goes first</p>
-                <PlayerFirst/>
-              </> : 
-              <>
-                <p>Player goes second</p>
-                <PlayerSecond />
-              </>
-              }
-    </div>
+      {!gameStarted ? 
+      <>
+        <p>Choose who will go first</p>
+        <div className='ButtonsPanel'>
+          <div className='ButtonPadding'><button className='ChooseButton' onClick={() => {setChosen(true)
+                                                                                          setGameStarted(true)}}>Player goes first</button></div>
+          <div className='ButtonPadding'><button className='ChooseButton' onClick={() => {setChosen(false)
+                                                                                          setGameStarted(true)
+                                                                                          }}>Player goes second</button></div>
+        </div>
+      </> : ''}
+      {chosen ? <p>Player goes first</p> : <p>Player goes second</p>}
+      {gameStarted ? <PlayerFirst status={chosen}/> : <div><p>Choose the regime to start the game</p></div>}
+  </div>
    );
 }
 
--- matches/src/pages/playerFirst.js
+++ matches/src/pages/playerFirst.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 
 const PlayerFirst = (props) => {
+
     const [matchesLeft, setMatchesLeft] = useState(25)
     const [matchesLeftYours, setMatchesLeftYours] = useState(0)
     const [matchesLeftAI, setMatchesLeftAI] = useState(0)
     
     let isGameEnded = false;
-  
+
     const calculateWinner = (number, matchesPlayer) => {
       if (number === 0) {
         if (matchesPlayer % 2 === 0) {
@@ -18,7 +19,7 @@ const PlayerFirst = (props) => {
         return 'Take matches';
       }
     }
-  
+
     const winner = calculateWinner(matchesLeft, matchesLeftYours)
   
     if (winner === 'You are winner' || winner === 'AI is winner') {
@@ -41,7 +42,11 @@ const PlayerFirst = (props) => {
         setMatchesLeftAI(matchesLeftAI => matchesLeftAI + 1)
       }
     }
-  
+
+    if (!props.status && matchesLeft === 25) {
+      takeMatchesAI(matchesLeft)
+    }
+
     return (
       <div className='App'>
           <div className='Title'>
@@ -52,7 +57,11 @@ const PlayerFirst = (props) => {
           </div>
           <div className='Matches'>{matchesLeft} ✏️</div>
           <div className='Winner'>{winner}</div>
-          {isGameEnded ? <div className='GameEnded'>Game Ended</div> :
+          {isGameEnded ? <div className='ButtonPadding'><button className='ChooseButton' onClick={() => {
+                                                                                                  setMatchesLeft(25)
+                                                                                                  setMatchesLeftYours(0)
+                                                                                                  setMatchesLeftAI(0)
+                                                                                                  }}>Play again</button></div> :
             <div className='ButtonsPanel'>
               {(matchesLeft >= 3) ? <div className='ButtonPadding'><button className='ChooseButton' onClick={() => {takeMatches(3)}}>✏️ ✏️ ✏️</button></div> : 
                 <div className='ButtonPadding'><button className='ChooseButton' disabled onClick={() => {takeMatches(3)}}>Not enough matches</button></div>}
